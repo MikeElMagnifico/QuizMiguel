e6359ee448952f6cd12a8946865f605a609e4573
alumno Secundaria <al3201@cedi.local>
2018-10-15 08:39:59 -0500

    Se creo toda la aplicación de disositivos con sus respectivos cards, funciones, y toggles

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BocinaPage } from '../bocina/bocina';
+import { TvPage } from '../tv/tv';
+import { SmartPage } from '../smart/smart';
+import { LampPage } from '../lamp/lamp';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  bocina = BocinaPage;
+  tv = TvPage;
+  smart = SmartPage;
+  lam = LampPage;
   constructor(public navCtrl: NavController) {
 
+  }
+  Bocina()
+  {
+    this.navCtrl.push(this.bocina);
+  }
+  TV()
+  {
+    this.navCtrl.push(this.tv);
+  }
+  Lampara()
+  {
+    this.navCtrl.push(this.lam);
+  }
+  Smartphone()
+  {
+    this.navCtrl.push(this.smart);
   }
 
 }
